7594ee624b4b666e65973ca4ba69f4fd26c58e73
Arun Motoori <arun.motoori@gmail.com>
2026-06-26 17:46:03 +0530

    Assignment - Methods Returning Data in Java

---
+++ 9. Assignment - Methods returning data in Java/DemoL.java
@@ -0,0 +1,14 @@
+
+public class DemoL {
+
+	public static void main(String[] args) {
+		
+		System.out.println(doubleTheNumber(4));
+
+	}
+	
+	public static int doubleTheNumber(int a) {
+		return a*2;
+	}
+
+}
