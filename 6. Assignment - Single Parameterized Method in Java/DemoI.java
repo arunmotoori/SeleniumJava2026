6f59dc82a8aa68ffc672b55f8ea1db3227610436
Arun Motoori <arun.motoori@gmail.com>
2026-06-26 14:17:09 +0530

    Assignment - Single Parameterized Method

---
+++ 6. Assignment - Single Parameterized Method in Java/DemoI.java
@@ -0,0 +1,15 @@
+
+public class DemoI {
+
+	public static void main(String[] args) {
+		welcomeUser("Arun");
+		welcomeUser("Prakash");
+	}
+	
+	public static void welcomeUser(String name) {
+		System.out.println("Welcome "+name);
+	}
+
+}
+
+
