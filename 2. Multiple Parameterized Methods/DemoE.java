a067354eae78f4c6ac328fba4817b8dc02d4c574
Arun Motoori <arun.motoori@gmail.com>
2026-06-26 10:40:13 +0530

    Multiple Parameterized Methods

---
+++ 2. Multiple Parameterized Methods/DemoE.java
@@ -0,0 +1,13 @@
+
+public class DemoE {
+
+	public static void main(String[] args) {
+		addTwoNumbers(9,5);
+	}
+	
+	public static void addTwoNumbers(int a,int b) {
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
