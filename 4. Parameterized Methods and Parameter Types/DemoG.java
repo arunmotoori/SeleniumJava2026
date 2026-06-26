181b9d9df96efe42711ce65f1ba507cdeae2367f
Arun Motoori <arun.motoori@gmail.com>
2026-06-26 11:58:37 +0530

    Parameterized Methods and Parameter Types

---
+++ 4. Parameterized Methods and Parameter Types/DemoG.java
@@ -0,0 +1,22 @@
+
+public class DemoG {
+
+	public static void main(String[] args) {
+		
+		sampleA(9,5.5,'s',true,(byte)3,4.32f,"Arun");
+
+	}
+	
+	public static void sampleA(int a,double b,char c,boolean d,byte e,float g,String h) {
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(g);
+		System.out.println(h);
+		
+	}
+
+}
