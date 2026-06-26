1a27814d1e46899ed438ae25f18d456d542cab4f
Arun Motoori <arun.motoori@gmail.com>
2026-06-26 14:15:18 +0530

    Purpose of Methods

---
+++ 5. Purpose of Methods in Java/DemoH.java
@@ -0,0 +1,19 @@
+
+public class DemoH {
+
+	public static void main(String[] args) {
+		
+		add(5,4);
+		add(9,5);
+		add(3,7);
+		add(8,4);
+		
+	}
+	
+	public static void add(int a,int b) {
+		int sum = a+b;
+		System.out.println("Sum of "+a+" and "+b+" is: "+sum);
+	}
+	
+}
+
