2e64ebf99106f048c830334886348429661506fd
Arun Motoori <arun.motoori@gmail.com>
2026-06-28 10:59:24 +0530

    Using class as a template in Java Updated

--- 15. Using Class as a template in Java/Car.java
+++ 15. Using Class as a template in Java/Car.java
@@ -1,4 +1,29 @@
 
 public class Car {
-
+	
+	String company;
+	String model;
+	String color;
+	int price;
+	double milage;
+	int wheels;
+	
+	public void startCar() {
+		System.out.println(company+" car started");	
+	}
+	
+	public void stopCar() {
+		System.out.println(company+" car stopped");
+	}
+	
+	public void carDetails() {
+		System.out.println("Company of the car is "+company);
+		System.out.println("Model of the car is "+model);
+		System.out.println("Color of the car is "+color);
+		System.out.println("Price of the car is "+price);
+		System.out.println("Milage of the car is "+milage);
+		System.out.println("Wheels of the car are "+wheels);
+		System.out.println("------------------------------");
+	}
+	
 }
