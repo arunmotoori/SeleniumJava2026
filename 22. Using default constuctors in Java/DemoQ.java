b7addb7e1ead36e643927f1dedac012ef2e449b9
Arun Motoori <arun.motoori@gmail.com>
2026-06-29 16:16:48 +0530

    Using default constructors in Java

---
+++ 22. Using default constuctors in Java/DemoQ.java
@@ -0,0 +1,17 @@
+
+public class DemoQ {
+	
+	//Java will maintain an inbuilt default constructor
+	//hidden inbuilt default empty consturctor
+//	DemoQ(){
+//		
+//	}
+	
+	DemoQ(int a,int b){
+		System.out.println("Inside constructor "+(a+b));
+	}
+	
+
+}
+
+
---
+++ 22. Using default constuctors in Java/SampleB.java
@@ -0,0 +1,10 @@
+
+public class SampleB {
+
+	public static void main(String[] args) {
+		
+		new DemoQ(5,4);
+
+	}
+
+}
