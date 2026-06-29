8231e560013cb7094a9fa1eafa9409702fd003c9
Arun Motoori <arun.motoori@gmail.com>
2026-06-29 17:01:20 +0530

    Accessing instance variables and instance methods outside the class

---
+++ 23. Accessing instance variables nd instance methods outside the class in Java/DemoR.java
@@ -0,0 +1,15 @@
+
+public class DemoR {
+	
+	int a = 5;
+	int b = 10;
+	
+	public void sampleA() {
+		System.out.println("Inside sampleA");
+	}
+	
+	public void sampleB() {
+		System.out.println("Inside sampleB");
+	}
+	
+}
---
+++ 23. Accessing instance variables nd instance methods outside the class in Java/DemoS.java
@@ -0,0 +1,16 @@
+
+public class DemoS {
+
+	public static void main(String[] args) {
+		
+		DemoR dr = new DemoR();
+		
+		System.out.println(dr.a);
+		System.out.println(dr.b);
+		
+		dr.sampleA();
+		dr.sampleB();
+
+	}
+
+}
