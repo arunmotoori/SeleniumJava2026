974e3aaa10988e9253e078d5142e6ccb9a3429d7
Arun Motoori <arun.motoori@gmail.com>
2026-06-29 17:28:33 +0530

    Accessing static variables and static methods outside the class in Java

---
+++ 24. Accessing static variables and static methods outside the class in Java/DemoT.java
@@ -0,0 +1,15 @@
+
+public class DemoT {
+	
+	static int a = 5;
+	static int b = 10;
+	
+	public static void sampleA() {
+		System.out.println("Inside sampleA method");
+	}
+	
+	public static void sampleB() {
+		System.out.println("Inside sampleB method");
+	}
+
+}
---
+++ 24. Accessing static variables and static methods outside the class in Java/DemoU.java
@@ -0,0 +1,14 @@
+
+public class DemoU {
+
+	public static void main(String[] args) {
+		
+		System.out.println(DemoT.a);
+		System.out.println(DemoT.b);
+		
+		DemoT.sampleA();
+		DemoT.sampleB();
+
+	}
+
+}
