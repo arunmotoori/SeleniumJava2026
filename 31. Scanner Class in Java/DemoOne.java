433ec0de97b6a0fbbf14a0496da3be8fcede9ca8
Arun Motoori <arun.motoori@gmail.com>
2026-06-30 19:23:41 +0530

    Scanner Class in Java

---
+++ 31. Scanner Class in Java/DemoOne.java
@@ -0,0 +1,24 @@
+package scannerpack;
+
+import java.util.Scanner;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter first number:");
+		int num1 = scanner.nextInt();
+		System.out.println("Enter second number:");
+		int num2 = scanner.nextInt();
+	
+		int sum = num1+num2;
+		
+		System.out.println("Sum is "+sum);
+		
+		scanner.close();
+		
+	}
+
+}
