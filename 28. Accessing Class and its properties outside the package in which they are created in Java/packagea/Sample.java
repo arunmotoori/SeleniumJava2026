e413598ba0b74b0f05fb75e5aa348cdb198b073c
Arun Motoori <arun.motoori@gmail.com>
2026-06-30 13:20:06 +0530

    Accessing class and its properties outside the package in Java

---
+++ 28. Accessing Class and its properties outside the package in which they are created in Java/packagea/Sample.java
@@ -0,0 +1,16 @@
+package packagea;
+
+public class Sample {
+	
+	public int a = 5;
+	public static int b = 10;
+	
+	public void sampleA() {
+		System.out.println("Inside sampleA method");
+	}
+	
+	public static void sampleB() {
+		System.out.println("Inside sampleB method");
+	}
+
+}
