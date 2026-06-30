8695767988bea2bfc64f08f52226c5d263850a25
Arun Motoori <arun.motoori@gmail.com>
2026-06-30 17:29:41 +0530

    User defined classes and pre-defined classes in Java

---
+++ 29. User defined Classes and Pre-defined Classes in Java/DemoFour.java
@@ -0,0 +1,14 @@
+package basicspack;
+
+public class DemoFour {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		
+		String str = "Arun";
+		System.out.println(str);
+		
+	}
+
+}
