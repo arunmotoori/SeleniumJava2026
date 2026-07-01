172f33b20c6058e6ae0a9467d79f5b061c6e8e01
Arun Motoori <arun.motoori@gmail.com>
2026-07-01 19:33:27 +0530

    Can we create duplicate methods inside same class in Java

---
+++ 40. Can we create duplicate methods inside the same class in Java/Demo.java
@@ -0,0 +1,13 @@
+package packagec;
+
+public class Demo {
+	
+	public void sample() {
+		System.out.println("sample method one");
+	}
+	
+	public void sample() {
+		System.out.println("sample method two");
+	}
+
+}
