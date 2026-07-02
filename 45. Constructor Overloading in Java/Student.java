ca7bf8cf9ec6ec0ac8f42874473ee19ac59b3898
Arun Motoori <arun.motoori@gmail.com>
2026-07-02 15:27:16 +0530

    Constructor Overloading

---
+++ 45. Constructor Overloading in Java/Student.java
@@ -0,0 +1,28 @@
+package packagec;
+
+public class Student {
+	
+	
+	public Student() {
+		
+	}
+	
+	public Student(int age) {
+		
+	}
+	
+	public Student(double age) {
+		
+	}
+	
+	public Student(int age,double marks) { 
+		
+	}
+	
+	public Student(double marks,int age) {  
+		
+	}
+	
+	
+}
+
