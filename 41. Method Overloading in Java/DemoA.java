73bcaf389cdbdbe8770d71a712605e64c46e56f0
Arun Motoori <arun.motoori@gmail.com>
2026-07-02 11:58:28 +0530

    Method Overloading

--- 41. Method Overloading in Java/DemoA.java
+++ 41. Method Overloading in Java/DemoA.java
@@ -1,5 +1,33 @@
 package packagec;
 
 public class DemoA {
+	
+	public void sample() {
+		System.out.println("Inside sample method");
+	}
+	
+	public void sample(int a) {
+		System.out.println("Inside sample method "+a);
+	}
+	
+	public void sample(double a) {
+		System.out.println("Inside sample method "+a);
+	}
+	
+	public void sample(int a,int b) {
+		System.out.println("Inside sample method "+a);
+	}
+	
+	public void sample(int a,double b) {
+		System.out.println("Inside sample method "+a);
+	}
+	
+	public void sample(double a,double b) {
+		System.out.println("Inside sample method "+a);
+	}
+	
+	public void sample(double a,int b) {
+		System.out.println("Inside sample method "+a);
+	}
 
 }
