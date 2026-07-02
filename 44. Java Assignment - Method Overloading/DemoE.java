a0793a37d7c3343aef94ef64cb7d1b0c6af35477
Arun Motoori <arun.motoori@gmail.com>
2026-07-02 14:10:05 +0530

    Java Assignment - Method Overloading

---
+++ 44. Java Assignment - Method Overloading/DemoE.java
@@ -0,0 +1,17 @@
+package packagec;
+
+public class DemoE {
+	
+	public void area(double radius) {
+		System.out.println("Area of the circle is "+(3.14*radius*radius));
+	}
+	
+	public void area(int side) {
+		System.out.println("Area of the square is "+(side*side));
+	}
+	
+	public void area(int height,int width) {
+		System.out.println("Area of the rectangle is  "+(height*width));
+	}
+
+}
---
+++ 44. Java Assignment - Method Overloading/SampleE.java
@@ -0,0 +1,15 @@
+package packagec;
+
+public class SampleE {
+
+	public static void main(String[] args) {
+		
+		DemoE de = new DemoE();
+		
+		de.area(3.5);
+		de.area(4);
+		de.area(3,4);
+
+	}
+
+}
