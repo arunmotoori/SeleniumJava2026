fc4ef8b23c91f33d91cf1245dce25314ea0c2867
Arun Motoori <arun.motoori@gmail.com>
2026-07-09 16:19:21 +0530

    udpated

--- 68. non-static methods can access both static and non-static stuff direclty in Java/ClassX.java
+++ 68. non-static methods can access both static and non-static stuff direclty in Java/ClassX.java
@@ -1,5 +1,25 @@
 package modifierspack.staticthree;
 
 public class ClassX {
+	
+	static int wheels = 4; // static global variable
+	String company = "Honda"; // non-static global variable
+	
+	public void sampleOne() {
+		
+		System.out.println(wheels);
+		System.out.println(company);
+		sampleTwo();
+		sampleThree();
+		
+	}
+	
+	public static void sampleTwo() {
+		System.out.println("Inside sampleTwo static method");
+	}
+	
+	public void sampleThree() {
+		System.out.println("Inside sampleThree non-static method");
+	}
 
 }
