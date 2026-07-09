0c135fd8023f8e54d5d8840a3612965fc04de81d
Arun Motoori <arun.motoori@gmail.com>
2026-07-09 10:56:54 +0530

    static non-access modifier

---
+++ 65. static non-access modifier in Java/SampleA.java
@@ -0,0 +1,27 @@
+package classobjectpack;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		
+		Car kC = new Car("Kia", "Carrens", "Black", 2500000, 23.45); // Object will be created
+		
+		kC.startCar();
+		kC.stopCar();
+		kC.carDetails();
+		
+		Car hAmz = new Car("Honda", "Amaze", "Grey", 900000, 14);
+		
+		hAmz.startCar();
+		hAmz.stopCar();
+		hAmz.carDetails();
+		
+		Car mS = new Car("Maruti", "Swift", "Red",800000, 18);
+		
+		mS.startCar();
+		mS.stopCar();
+		mS.carDetails();
+
+	}
+
+}
