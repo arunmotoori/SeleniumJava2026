ad398fad9ccee6a3df031f02fbb2c01c28a4bcdc
Arun Motoori <arun.motoori@gmail.com>
2026-07-11 16:58:13 +0530

    Objects cannot be created for abstract classes in Java

---
+++ 73. Objects cannot be created for abstract classes in Java/abstarctwo/ClassX.java
@@ -0,0 +1,40 @@
+package modifierspack.abstarctwo;
+
+public abstract class ClassX {
+	
+	public abstract void sampleA();
+	
+	public abstract void sampleB();
+	
+	public abstract void sampleC();
+	
+	public void sampleD() {
+		System.out.println("SampleD");
+	}
+	
+	public void sampleE() {
+		System.out.println("SampleE");
+	}
+
+}
+
+class ClassY extends ClassX {
+
+	@Override
+	public void sampleA() {
+		System.out.println("SampleA");
+	}
+
+	@Override
+	public void sampleB() {
+		System.out.println("SampleB");
+	}
+
+	@Override
+	public void sampleC() {
+		System.out.println("SampleC");
+	}
+
+}
+
+
---
+++ 73. Objects cannot be created for abstract classes in Java/abstarctwo/Demo.java
@@ -0,0 +1,17 @@
+package modifierspack.abstarctwo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		//ClassX cx = new ClassX();
+		ClassY cy = new ClassY();
+		cy.sampleA();
+		cy.sampleB();
+		cy.sampleC();
+		cy.sampleD();
+		cy.sampleE();
+
+	}
+
+}
