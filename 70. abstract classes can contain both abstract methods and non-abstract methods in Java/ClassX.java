69b95706581e4f7439d5fbf102083f44d5378630
Arun Motoori <arun.motoori@gmail.com>
2026-07-11 11:51:04 +0530

    abstract classes can contain both abstract and non-abstract methods in Java

--- 70. abstract classes can contain both abstract methods and non-abstract methods in Java/ClassX.java
+++ 70. abstract classes can contain both abstract methods and non-abstract methods in Java/ClassX.java
@@ -1,5 +1,21 @@
 package modifierspack.abstarctwo;
 
-public class ClassX {
+public abstract class ClassX {
+	
+	public abstract void sampleA();
+	
+	public abstract void sampleB();
+	
+	public abstract void sampleC();
+	
+	public void sampleD() {
+		System.out.println("SampleD");
+	}
+	
+	public void sampleE() {
+		System.out.println("SampleE");
+	}
 
 }
+
+
