1a8776a352301c0635fee77753a623a7385f0c9c
Arun Motoori <arun.motoori@gmail.com>
2026-07-13 12:57:07 +0530

    Methods inside an interface are by default abstract methods

---
+++ 77. Methods inside an interface are by default abstract methods in Java/InterfaceA.java
@@ -0,0 +1,15 @@
+package interfacepack;
+
+public interface InterfaceA {
+	
+	public void sampleA();
+	public void sampleB();
+	public void sampleC();
+	public void sampleD();
+	public void sampleE() {
+		
+		
+	}
+	
+}
+
