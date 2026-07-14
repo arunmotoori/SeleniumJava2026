046ae72c986d953dcdbab3cdc29c8ec364fb9c19
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 12:55:03 +0530

    Getter and Setter Methods in Java

---
+++ 87. Getter and Setter Methods in Java/encapsulationpack/Car.java
@@ -0,0 +1,24 @@
+package encapsulationpack;
+
+public class Car {
+	
+	private String company = "Honda";
+	private int wheels = 4;
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	public int getWheels() {
+		return wheels;
+	}
+	
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
+
+}
---
+++ 87. Getter and Setter Methods in Java/encapsulationpack/Demo.java
@@ -0,0 +1,17 @@
+package encapsulationpack;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Car c = new Car();
+		
+		System.out.println(c.getCompany());
+		System.out.println(c.getWheels());
+		c.setCompany("Kia");
+		c.setWheels(8);
+		System.out.println(c.getCompany());
+		System.out.println(c.getWheels());
+	}
+
+}
