febe71bd9ac96e772130056ed0d096efb5d86319
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 19:23:35 +0530

    Initializing Single Dimensional Arrays in Java

---
+++ 94. Initializing Single Dimensional Arrays in Java/DemoThree.java
@@ -0,0 +1,21 @@
+package arrayspack;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		//Creating single dimensional array
+		int[] ar = new int[6];
+		
+		//Initialing the single dimensional array
+		ar[0] = 9;
+		ar[1] = 5;
+		ar[2] = 3;
+		ar[3] = 7;
+		ar[4] = 1;
+		ar[5] = 2;
+
+	}
+
+}
+
