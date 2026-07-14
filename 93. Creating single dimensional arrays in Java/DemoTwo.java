0d12e9df7674922e545655883c123056b321bdfd
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 19:05:18 +0530

    Creating single dimensional arrays in Java

---
+++ 93. Creating single dimensional arrays in Java/DemoTwo.java
@@ -0,0 +1,15 @@
+package arrayspack;
+
+public class DemoTwo {
+
+	public static void main(String[] args) {
+		
+		//Two ways of creating single dimensional arrays in Java
+		int[] ar = new int[6];
+		
+		int br[] = new int[3];
+		
+	}
+
+}
+
