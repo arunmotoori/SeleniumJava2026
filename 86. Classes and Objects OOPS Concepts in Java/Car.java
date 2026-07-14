c48a524b9db8318142a5fa7e6748083dc7f8e96e
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 12:23:38 +0530

    Classes and Objects OOPS Concepts in Java

---
+++ 86. Classes and Objects OOPS Concepts in Java/Car.java
@@ -0,0 +1,42 @@
+package classobjectpack;
+
+public class Car {
+	
+	String company;
+	String model;
+	String color;
+	int price;
+	double milage;
+	static int wheels = 4;
+	
+	public Car(String company,String model,String color,int price,double milage) {
+		this.company = company;
+		this.model = model;
+		this.color = color;
+		this.price = price;
+		this.milage = milage;
+	}
+	
+	public void startCar() {
+		System.out.println(company+" car started");	
+	}
+	
+	public void stopCar() {
+		System.out.println(company+" car stopped");
+	}
+	
+	public void carDetails() {
+		System.out.println("Company of the car is "+company);
+		System.out.println("Model of the car is "+model);
+		System.out.println("Color of the car is "+color);
+		System.out.println("Price of the car is "+price);
+		System.out.println("Milage of the car is "+milage);
+		System.out.println("Wheels of the car are "+wheels);
+		System.out.println("------------------------------");
+	}
+	
+	public static void printCarName() {
+		System.out.println("Name of the car is xyz");
+	}
+	
+}
---
+++ 86. Classes and Objects OOPS Concepts in Java/SampleA.java
@@ -0,0 +1,30 @@
+package classobjectpack;
+
+public class SampleA {
+
+	public static void main(String[] args) {
+		
+		Car kC = new Car("Kia", "Carrens", "Black", 2500000, 23.45); // Object will be created
+		
+		kC.startCar();
+		kC.stopCar();
+		kC.carDetails();
+		
+		System.out.println(Car.wheels);
+		Car.printCarName();
+		
+		Car hAmz = new Car("Honda", "Amaze", "Grey", 900000, 14);
+		
+		hAmz.startCar();
+		hAmz.stopCar();
+		hAmz.carDetails();
+		
+		Car mS = new Car("Maruti", "Swift", "Red",800000, 18);
+		
+		mS.startCar();
+		mS.stopCar();
+		mS.carDetails();
+
+	}
+
+}
