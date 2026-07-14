1d12ea4c5d4b38d7142be429c7a7a3bc13d2add0
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 19:39:43 +0530

    Accessing data stored in single dimensional arrays in Java

---
+++ 95. Accessing data stored in Single Dimensional Arrays in Java/DemoThree.java
@@ -0,0 +1,29 @@
+package arrayspack;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		//Creating single dimensional array
+		int[] ar = new int[6];
+		
+		//Initialing the single dimensional array
+		ar[0] = 9;
+		ar[1] = 5;
+		ar[2] = 3;
+		ar[3] = 7;
+		ar[4] = 1;
+		ar[5] = 2;
+		
+		//Access the data stored in the single dimensional array
+		System.out.println(ar[0]); //9 
+		System.out.println(ar[1]); //5
+		System.out.println(ar[2]); //3
+		System.out.println(ar[3]); //7
+		System.out.println(ar[4]); //1
+		System.out.println(ar[5]); //2
+		
+	}
+
+}
+
