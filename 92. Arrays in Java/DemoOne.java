9077585ca254aa89517188f89c724857285ea835
Arun Motoori <arun.motoori@gmail.com>
2026-07-14 17:37:29 +0530

    Arrays in Java

---
+++ 92. Arrays in Java/DemoOne.java
@@ -0,0 +1,15 @@
+package arrayspack;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		System.out.println(a);
+		
+		//int b = 5,7,9,2,4,3,6; - This is not possible
+		
+
+	}
+
+}
