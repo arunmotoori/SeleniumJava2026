3680a55b87b1ea9d969ce26c95623c760cd83dc7
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 11:29:41 +0530

    Shortucut represent of Single Dimensional Arrays in Java

---
+++ 98. Shortcut representation of Single Dimensional Arrays in Java/DemoThree.java
@@ -0,0 +1,22 @@
+package arrayspack;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,5,3,7,1,2};
+		
+		for(int i=0;i<6;i++) {
+			System.out.println(ar[i]);
+		}
+		
+		System.out.println("----------------");
+		
+		for(int e:ar) {
+			System.out.println(e);
+		}
+
+	}
+
+}
+
