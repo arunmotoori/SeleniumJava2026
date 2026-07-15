ecc211ea3ea89b8d93ebeafbfd90740d0e96b08c
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 09:05:50 +0530

    Using for loop with Single Dimensional Arrays in Java

---
+++ 96. Using for loop with Single Dimensional Arrays in Java/DemoThree.java
@@ -0,0 +1,26 @@
+package arrayspack;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		//Creating single dimensional array
+		int[] ar = new int[6];
+		
+		//Initialing the single dimensional array
+		ar[0] = 9;
+		ar[1] = 5;
+		ar[2] = 3;
+		ar[3] = 7;
+		ar[4] = 1;
+		ar[5] = 2;
+		
+		//Access the data stored in the single dimensional array
+		for(int i=0;i<6;i++) {  // 6
+			System.out.println(ar[i]); // 9 5 3 7 1 2 
+		}
+		
+	}
+
+}
+
