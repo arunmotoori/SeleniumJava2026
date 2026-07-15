03223dcbefb09c322f0bb441241f7834f91bcd5a
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 16:01:07 +0530

    Wrapper Classes in Java

---
+++ 102. Wrapper Classes in Java/DemoOne.java
@@ -0,0 +1,38 @@
+package wrapperpack;
+
+import java.util.ArrayList;
+
+public class DemoOne {
+
+	public static void main(String[] args) {
+		
+		byte a = 5;
+		short b = 10;
+		int c = 15;
+		long d = 25L;
+		float e = 1.23F;
+		double f = 3.24;
+		char g = 's';
+		boolean h = true;
+		
+		Byte i = 35;
+		Short j = 40;
+		Integer k = 45;
+		Long l = 50L;
+		Float m = 4.23F;
+		Double n = 3.33D;
+		Character o = '$';
+		Boolean p = false;
+		
+		i = a;
+		
+		System.out.println(i);
+		
+		c = k;
+		
+		System.out.println(c);
+		
+		ArrayList<Integer> alist = new ArrayList<Integer>();
+	}
+
+}
