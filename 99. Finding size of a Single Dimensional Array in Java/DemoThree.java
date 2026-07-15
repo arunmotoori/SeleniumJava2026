acd622493087ee56d4d4bbee87310975af62ce90
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 11:53:35 +0530

    Finding size of a Single Dimensional Array in Java

---
+++ 99. Finding size of a Single Dimensional Array in Java/DemoThree.java
@@ -0,0 +1,20 @@
+package arrayspack;
+
+public class DemoThree {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,5,3,7,1,2};
+		
+		int size = ar.length;
+		
+		System.out.println("Size of the array is "+size);
+		
+		for(int i=0;i<size;i++) {
+			System.out.println(ar[i]);
+		}
+
+	}
+
+}
+
