e0832702e898cde6fa2fe55816c456ae72a38410
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 19:21:27 +0530

    Initializing Two Dimensional Arrays in Java

---
+++ 107. Initializing Two Dimensional Arrays in Java/DemoTwo.java
@@ -0,0 +1,26 @@
+package twodarraypack;
+
+public class DemoTwo {
+
+	public static void main(String[] args) {
+		
+		int[][] ar = new int[3][4];
+		
+		ar[0][0] = 9;
+		ar[0][1] = 11;
+		ar[0][2] = 5;
+		ar[0][3] = 7;
+		
+		ar[1][0] = 3;
+		ar[1][1] = 6;
+		ar[1][2] = 8;
+		ar[1][3] = 2;
+		
+		ar[2][0] = 4;
+		ar[2][1] = 1;
+		ar[2][2] = 15;
+		ar[2][3] = 14;
+
+	}
+
+}
