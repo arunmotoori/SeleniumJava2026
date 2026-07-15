ea22c2808815d1c682ca54fb182de1b8e567ad0e
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 19:45:10 +0530

    Accessing elements from Two Dimensional Array in Java

---
+++ 108. Accessing elements from Two Dimensional Array in Java/DemoTwo.java
@@ -0,0 +1,39 @@
+package twodarraypack;
+
+public class DemoTwo {
+
+	public static void main(String[] args) {
+		
+		int[][] ar = new int[3][4];
+		
+		ar[0][0] = 9;
+		ar[0][1] = 11;
+		ar[0][2] = 5;
+		ar[0][3] = 7;
+		
+		ar[1][0] = 3;
+		ar[1][1] = 6;
+		ar[1][2] = 8;
+		ar[1][3] = 2;
+		
+		ar[2][0] = 4;
+		ar[2][1] = 1;
+		ar[2][2] = 15;
+		ar[2][3] = 14;
+		
+		System.out.println(ar[0][0]);
+		System.out.println(ar[0][1]);
+		System.out.println(ar[0][2]);
+		System.out.println(ar[0][3]);
+		System.out.println(ar[1][0]);
+		System.out.println(ar[1][1]);
+		System.out.println(ar[1][2]);
+		System.out.println(ar[1][3]);
+		System.out.println(ar[2][0]);
+		System.out.println(ar[2][1]);
+		System.out.println(ar[2][2]);
+		System.out.println(ar[2][3]);
+
+	}
+
+}
