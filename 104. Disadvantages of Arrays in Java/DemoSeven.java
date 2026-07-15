f23ba99878d1578202a4325b5f7ca4ce10e41f2f
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 17:14:13 +0530

    Disadvantages of Arrays in Java

---
+++ 104. Disadvantages of Arrays in Java/DemoSeven.java
@@ -0,0 +1,13 @@
+package arrayspack;
+
+public class DemoSeven {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,1.5,true,"Arun",'s',3.2F,13L};
+
+	}
+
+}
+
+
---
+++ 104. Disadvantages of Arrays in Java/DemoSix.java
@@ -0,0 +1,25 @@
+package arrayspack;
+
+import java.util.Arrays;
+
+public class DemoSix {
+
+	public static void main(String[] args) {
+		
+		int[] ar = new int[3];
+		
+		ar[0] = 9;
+		ar[1] = 15;
+		ar[2] = 3;
+		
+		System.out.println(Arrays.toString(ar));
+		
+		ar[3] = 6;
+		
+		System.out.println(Arrays.toString(ar));
+
+	}
+
+}
+
+
