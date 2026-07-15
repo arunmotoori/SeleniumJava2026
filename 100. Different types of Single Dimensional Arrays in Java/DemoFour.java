b801d3165389ecc16284d2959e4f3cd94ee070a3
Arun Motoori <arun.motoori@gmail.com>
2026-07-15 12:41:40 +0530

    Different types of Single Dimensional Arrays in Java

---
+++ 100. Different types of Single Dimensional Arrays in Java/DemoFour.java
@@ -0,0 +1,22 @@
+package arrayspack;
+
+public class DemoFour {
+
+	public static void main(String[] args) {
+		
+		byte[] br = {9,1,4,2};
+		short[] sr = {4,2,6,8};
+		int[] ir = {9,6,2,8};
+		long[] lr = {9,8L,3l};
+		
+		float[] fr = {1.2f,3.2F};
+		double[] dr = {3.2,4.6D,7.6d,1.2f,2.3F};
+		
+		char[] cr = {'s','1','#'};
+		boolean[] bor = {true,false,false,true,true};
+		
+		String[] star = {"Arun","Varun","Motoori"};
+	
+	}
+
+}
