84e584db59dfb3f12564cbabeefae84183a534b5
Arun Motoori <arun.motoori@gmail.com>
2026-07-16 16:56:32 +0530

    ArrayIndexOutOfBoundsException in Java

---
+++ 115. ArrayIndexOutOfBoundsException in Java/DemoFour.java
@@ -0,0 +1,21 @@
+package twodarraypack;
+
+import java.util.Arrays;
+
+public class DemoFour {
+
+	public static void main(String[] args) {
+		
+		int[] ar = new int[3];
+		
+		ar[0] = 9;
+		ar[1] = 5;
+		ar[2] = 7;
+		
+		System.out.println(Arrays.toString(ar));
+		
+		System.out.println(ar[3]);
+
+	}
+
+}
