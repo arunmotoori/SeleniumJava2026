64e15618ad2f77385f4ca5085536d72525ee14c9
Arun Motoori <arun.motoori@gmail.com>
2026-07-16 11:02:20 +0530

    Accessing data from Two Dimensional Array using for loop in Java

---
+++ 109. Accessing data from Two Dimensional Array using for loop in Java/DemoTwo.java
@@ -0,0 +1,36 @@
+package twodarraypack;
+
+public class DemoTwo {
+
+	public static void main(String[] args) {
+		
+		int[][] ar = new int[3][4];
+		
+		ar[0][0] = 9;
+		ar[0][1] = 11;
+		ar[0][2] = 5;
+		ar[0][3] = 7;
+		
+		ar[1][0] = 3;
+		ar[1][1] = 6;
+		ar[1][2] = 8;
+		ar[1][3] = 2;
+		
+		ar[2][0] = 4;
+		ar[2][1] = 1;
+		ar[2][2] = 15;
+		ar[2][3] = 14;
+		
+		for(int r=0;r<3;r++) {  // 3
+			
+			for(int c=0;c<4;c++) { // 4
+				
+				System.out.println(ar[r][c]); // 9 11 5 7 3 6 8 2 4 1 15 14 
+				
+			}
+			
+		}
+
+	}
+
+}
