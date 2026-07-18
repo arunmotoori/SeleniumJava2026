f7543c806406e691ee6595d34e96c08fdb0898a4
Arun Motoori <arun.motoori@gmail.com>
2026-07-18 14:45:50 +0530

    Printing few elements of a single dimensional array

---
+++ 119. Assignment - Printing few elements of a Single Dimensional Array in Java/DemoB.java
@@ -0,0 +1,17 @@
+package twodarraypack;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,2,5,7,6,2,4};
+		
+		for(int i=1;i<=3;i++) {
+			
+			System.out.println(ar[i]);
+			
+		}
+
+	}
+
+}
