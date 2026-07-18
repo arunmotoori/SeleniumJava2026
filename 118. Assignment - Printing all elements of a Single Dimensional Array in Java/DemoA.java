409b89532565fb06f1190779e1b77a4ce3f2375d
Arun Motoori <arun.motoori@gmail.com>
2026-07-18 14:04:16 +0530

    Assignment - Printing all elements of a Single Dimensional Array in Java

---
+++ 118. Assignment - Printing all elements of a Single Dimensional Array in Java/DemoA.java
@@ -0,0 +1,17 @@
+package twodarraypack;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,2,7,5,6,8,4};
+		
+		for(int e:ar) {
+			System.out.println(e);
+		}
+
+	}
+
+}
+
+
