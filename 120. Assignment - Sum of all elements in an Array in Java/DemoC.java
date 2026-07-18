b2c52c258fcc986840ee0b2357cd998c4565fd0d
Arun Motoori <arun.motoori@gmail.com>
2026-07-18 16:17:23 +0530

    Assignment - Sum of all elements in an Array in Java

---
+++ 120. Assignment - Sum of all elements in an Array in Java/DemoC.java
@@ -0,0 +1,19 @@
+package twodarraypack;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,2,5,4,7,6,8};
+		
+		int sum = 0;  
+		
+		for(int e:ar) {
+			sum+=e; 
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
