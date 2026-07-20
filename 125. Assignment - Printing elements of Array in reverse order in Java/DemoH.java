e21398902c13d89add74f17533775c05bcc2df44
Arun Motoori <arun.motoori@gmail.com>
2026-07-20 15:22:31 +0530

    Printing elements of array in reverse order in Java

---
+++ 125. Assignment - Printing elements of Array in reverse order in Java/DemoH.java
@@ -0,0 +1,15 @@
+package twodarraypack;
+
+public class DemoH {
+
+	public static void main(String[] args) {
+		
+		int[] ar = {9,3,6,2,7,5,4,8};
+		
+		for(int i=ar.length-1;i>=0;i--) {
+			System.out.print(ar[i]+" "); // 8 4 5 7 2 6 3 9 
+		}
+
+	}
+
+}
