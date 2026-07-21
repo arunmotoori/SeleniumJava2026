28275898cf89b7a2ecd6217fc6a798dd4b0d6684
Arun Motoori <arun.motoori@gmail.com>
2026-07-21 22:19:00 +0530

    charAt(index) pre-defined method of String class in Java

---
+++ 133. charAt(index) pre-defined method of String class in Java/DemoC.java
@@ -0,0 +1,18 @@
+package stringspack;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		
+		String str = "Java Programming";
+		
+		char c = str.charAt(7);
+		System.out.println(c);
+		
+		System.out.println(str.charAt(9));
+
+	}
+
+}
+
+
