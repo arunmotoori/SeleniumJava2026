8d151fdd0f21b2b1c74e10fda02c846951ef7206
Arun Motoori <arun.motoori@gmail.com>
2026-07-21 16:42:43 +0530

    Creating Strings in Java

---
+++ 131. Creating Strings in Java/DemoA.java
@@ -0,0 +1,18 @@
+package stringspack;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		String name = new String("Arun Motoori");
+		
+		System.out.println(name);
+		
+		String str = "Java";
+		
+		System.out.println(str);
+
+	}
+
+}
+
