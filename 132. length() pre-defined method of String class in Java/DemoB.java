58c66bb8781a2cb0b74d629b1d029f8f2d41d713
Arun Motoori <arun.motoori@gmail.com>
2026-07-21 21:55:24 +0530

    length() pre-defined method of String class in Java

---
+++ 132. length() pre-defined method of String class in Java/DemoB.java
@@ -0,0 +1,16 @@
+package stringspack;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		
+		String name = "Arun Motoori";
+		
+		System.out.println(name.length());
+		
+//		int size = name.length();
+//		System.out.println(size);
+
+	}
+
+}
