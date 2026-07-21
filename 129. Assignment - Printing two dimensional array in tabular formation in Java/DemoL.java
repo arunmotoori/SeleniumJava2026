c75b56ef2d2c3f09033b4855654a3e65d94d671e
Arun Motoori <arun.motoori@gmail.com>
2026-07-21 11:35:22 +0530

    Printing two dimesional array in tabular format in java

---
+++ 129. Assignment - Printing two dimensional array in tabular formation in Java/DemoL.java
@@ -0,0 +1,18 @@
+package twodarraypack;
+
+public class DemoL {
+
+	public static void main(String[] args) {
+		
+		char[][] ar = {{'v','a','r','u','n'},{'s','a','n','d','y'},{'m','e','e','n','a'}};
+		
+		for(char[] cr : ar) {
+			for(char c : cr) {
+				System.out.print(c+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
