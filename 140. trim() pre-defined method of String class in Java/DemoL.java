9ebe49150d6901dc10abd89d6204428843dd43cd
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 15:13:43 +0530

    trim() predefined method of String class in Java

---
+++ 140. trim() pre-defined method of String class in Java/DemoL.java
@@ -0,0 +1,16 @@
+package stringspack;
+
+public class DemoL {
+
+	public static void main(String[] args) {
+		
+		String str = "    Arun Motoori    ";
+		System.out.println(str);
+		System.out.println(str.length());
+		String s2 = str.trim();
+		System.out.println(s2);
+		System.out.println(s2.length());
+		
+	}
+
+}
