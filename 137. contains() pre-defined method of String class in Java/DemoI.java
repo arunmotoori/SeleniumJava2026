1a44d186be69a6b2f33d885eb615632f103855e2
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 12:28:00 +0530

    contains() pre-defined method of String class in Java

---
+++ 137. contains() pre-defined method of String class in Java/DemoI.java
@@ -0,0 +1,15 @@
+package stringspack;
+
+public class DemoI {
+
+	public static void main(String[] args) {
+		
+		String str = "I love Java Programming";
+		
+		System.out.println(str.contains("Java")); // true
+		System.out.println(str.contains("Python")); // false
+
+	}
+
+}
+
