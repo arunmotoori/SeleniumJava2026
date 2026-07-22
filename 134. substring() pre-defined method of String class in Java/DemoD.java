4ab97544226b6908ba72270b278b0b3b8d8876e3
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 09:56:12 +0530

    substring() pre-defined method of String class in Java

---
+++ 134. substring() pre-defined method of String class in Java/DemoD.java
@@ -0,0 +1,18 @@
+package stringspack;
+
+public class DemoD {
+
+	public static void main(String[] args) {
+		
+		String str = "Arun Motoori";
+		
+//		String str2 = str.substring(1);
+//		System.out.println(str2);
+		
+		System.out.println(str.substring(1));
+		System.out.println(str.substring(1,4));
+		System.out.println(str.substring(5,9));
+
+	}
+
+}
