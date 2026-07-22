81e2bcf9a13ecd0ef7fb558a6e30ce01794d2145
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 18:44:37 +0530

    concat() pre-defined method of String class in Java

---
+++ 144. concat() pre-defined method of String class in Java/DemoP.java
@@ -0,0 +1,23 @@
+package stringspack;
+
+public class DemoP {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Arun";
+		String lastName = "Motoori";
+		
+//		String s1 = firstName.concat(" ");
+//		System.out.println(s1);
+//		
+//		String s2 = s1.concat(lastName);
+//		System.out.println(s2);
+		
+//		String s3 = firstName.concat(" ").concat(lastName);
+//		System.out.println(s3);
+		
+		System.out.println(firstName.concat(" ").concat(lastName));
+
+	}
+
+}
