19f81b9453df25d13be6a47f2a277abed5e1755e
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 11:49:09 +0530

    equalsIgnorecase() pre-defined method of String class in Java

---
+++ 136. equalsIgnoreCase() pre-defined method of String class in Java/DemoH.java
@@ -0,0 +1,15 @@
+package stringspack;
+
+public class DemoH {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Arun";
+		String s2 = "arun";
+		
+		System.out.println(s1.equals(s2)); // false
+		System.out.println(s1.equalsIgnoreCase(s2)); // true
+
+	}
+
+}
