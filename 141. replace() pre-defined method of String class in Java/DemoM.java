9b11c0ac8412cbca6233a653b3349df05e5e2745
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 15:41:18 +0530

    replace() pre-defined method of String class in Java

---
+++ 141. replace() pre-defined method of String class in Java/DemoM.java
@@ -0,0 +1,22 @@
+package stringspack;
+
+public class DemoM {
+
+	public static void main(String[] args) {
+		
+		String str = "feet";
+		
+		String s2 = str.replace('e','o');
+		System.out.println(s2); // foot
+		
+		String str2 = "I love Java programming";
+		
+		String s3 = str2.replace("Java","Python");
+		System.out.println(s3);
+		
+
+	}
+
+}
+
+
