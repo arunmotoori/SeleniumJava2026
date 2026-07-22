8892212b7e99857d2f84986dea3d7b76397e3659
Arun Motoori <arun.motoori@gmail.com>
2026-07-22 13:22:18 +0530

    toUpperCase and toLowerCase methods of String class in Java

---
+++ 139. toUpperCase() and toLowerCase() methods of String class in Java/DemoK.java
@@ -0,0 +1,19 @@
+package stringspack;
+
+public class DemoK {
+
+	public static void main(String[] args) {
+		
+		String str = "Arun Motoori";
+		
+		String str2 = str.toUpperCase();
+		System.out.println(str2); // ARUN MOTOORI
+		
+		String str3 = str.toLowerCase();
+		System.out.println(str3); // arun motoori
+
+	}
+
+}
+
+
