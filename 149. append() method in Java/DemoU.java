370bc055dff8add0028c4b8a801d9c314cc84d1f
Arun Motoori <arun.motoori@gmail.com>
2026-07-23 18:03:02 +0530

    append() method in Java

---
+++ 149. append() method in Java/DemoU.java
@@ -0,0 +1,17 @@
+package stringspack;
+
+public class DemoU {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder("Arun");
+		
+		System.out.println(sb);
+		
+		sb.append(" Motoori");
+		
+		System.out.println(sb);
+
+	}
+
+}
