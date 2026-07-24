cfe34bafdebb34d9ac013a9ebf54409fe35842cb
Arun Motoori <arun.motoori@gmail.com>
2026-07-24 16:35:31 +0530

    reverse() method of StringBuilder and StringBuffer classes in Java

---
+++ 153. reverse() method of StringBuilder and StringBuffer classes in Java/DemoC.java
@@ -0,0 +1,17 @@
+package stringbuilderpack;
+
+public class DemoC {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder("Arun");
+		
+		System.out.println(sb);
+		
+		sb.reverse();
+		
+		System.out.println(sb);
+
+	}
+
+}
