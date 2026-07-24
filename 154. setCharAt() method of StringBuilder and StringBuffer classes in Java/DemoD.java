84585b28ba18e34d7ddc4d2194982a47607116b3
Arun Motoori <arun.motoori@gmail.com>
2026-07-24 17:17:38 +0530

    setCharAt method

---
+++ 154. setCharAt() method of StringBuilder and StringBuffer classes in Java/DemoD.java
@@ -0,0 +1,17 @@
+package stringbuilderpack;
+
+public class DemoD {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder("Java 8");
+		
+		System.out.println(sb);
+		
+		sb.setCharAt(5,'9');
+		
+		System.out.println(sb);
+
+	}
+
+}
