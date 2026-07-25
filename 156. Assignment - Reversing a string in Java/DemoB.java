7620f23ecae0086634c2473d9209f8edc8ae9f3f
Arun Motoori <arun.motoori@gmail.com>
2026-07-25 10:40:48 +0530

    Reversing a string in Java

---
+++ 156. Assignment - Reversing a string in Java/DemoB.java
@@ -0,0 +1,21 @@
+package stringspacktwo;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		
+		String str = "Arun Motoori";
+		
+		String rev = ""; 
+		
+		for(int i=str.length()-1;i>=0;i--) {
+			rev = rev + str.charAt(i);
+		}
+		
+		System.out.println(rev);
+
+	}
+
+}
+
+
