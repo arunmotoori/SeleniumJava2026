0886430941e5ad6241803a2caf0686aa06e37b37
Arun Motoori <arun.motoori@gmail.com>
2026-07-25 09:59:34 +0530

    Size of a string

---
+++ 155. Assignment - Finding size of a string in Java/DemoA.java
@@ -0,0 +1,15 @@
+package stringspacktwo;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		String str = "Core Java";
+		
+		int size = str.length();
+		
+		System.out.println("Size of string:"+str+" is "+size);
+
+	}
+
+}
