cce3abf2029f62dce5b75323dedfeb3093a2c3d5
Arun Motoori <arun.motoori@gmail.com>
2026-07-26 15:34:50 +0530

    What will happen when you don't handle exception in Java

---
+++ 162. What will happen when you dont handle exception in Java/DemoA.java
@@ -0,0 +1,23 @@
+package exceptionspack;
+import java.util.Scanner;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int a = scanner.nextInt();
+		System.out.println("Enter second number:");
+		int b = scanner.nextInt();
+		scanner.close();
+		
+		int c = a/b;
+		System.out.println(c);
+		
+		System.out.println("End of this program.");
+		
+	}
+
+}
+
