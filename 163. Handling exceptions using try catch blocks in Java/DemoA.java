9d324932415b4c2145fe1cd45b40eb556ae8bbf8
Arun Motoori <arun.motoori@gmail.com>
2026-07-26 17:18:54 +0530

    Handling exceptions in Java using try catch blocks

---
+++ 163. Handling exceptions using try catch blocks in Java/DemoA.java
@@ -0,0 +1,27 @@
+package exceptionspack;
+import java.util.Scanner;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int a = scanner.nextInt();
+		System.out.println("Enter second number:");
+		int b = scanner.nextInt();
+		scanner.close();
+		
+		try {
+			int c = a/b;
+			System.out.println(c);
+		}catch(ArithmeticException e) {
+			System.out.println("Exception got handled here");
+		}
+		
+		System.out.println("End of this program.");
+		
+	}
+
+}
+
