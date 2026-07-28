d8ecc03f4dd05fb2ef8392c65062ad07c8e149c8
Arun Motoori <arun.motoori@gmail.com>
2026-07-28 19:54:46 +0530

    Unchecked Exceptions in Java

---
+++ 169. Unchecked Exceptions in Java/DemoH.java
@@ -0,0 +1,19 @@
+package exceptionspack;
+
+public class DemoH {
+
+	public static void main(String[] args) {
+		
+//		int a = 9,b = 0,c;
+//		c = a/b;
+//		System.out.println(c);
+		
+		int[] ar= new int[2];
+		ar[0] = 9;
+		ar[1] = 5;
+		ar[2] = 7;
+		
+
+	}
+
+}
