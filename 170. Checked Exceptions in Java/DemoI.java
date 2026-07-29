23e1e925828f4051fa5246e7908a198390a09dc9
Arun Motoori <arun.motoori@gmail.com>
2026-07-29 10:26:05 +0530

    Checked Exceptions in Java

---
+++ 170. Checked Exceptions in Java/DemoI.java
@@ -0,0 +1,15 @@
+package exceptionspack;
+
+import java.io.FileReader;
+
+public class DemoI {
+
+	public static void main(String[] args) {
+		
+		//int c = 9/0;
+		
+		FileReader fr = new FileReader("abc.txt");
+
+	}
+
+}
