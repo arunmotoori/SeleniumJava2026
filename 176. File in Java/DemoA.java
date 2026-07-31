8835213b3d5f301603f3d3ec730f44a4b6e8c4c7
Arun Motoori <arun.motoori@gmail.com>
2026-07-31 19:10:36 +0530

    File in Java

---
+++ 176. File in Java/DemoA.java
@@ -0,0 +1,18 @@
+package filehandlingpack;
+
+import java.io.File;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		
+		//String str = "\\";
+		
+		//Represent a file in Java
+		//Here we have to provide a double backward slash or single forward slash
+		File file1 = new File("C:\\Users\\arunm\\OneDrive\\Desktop\\FolderA\\abc.txt");
+
+	}
+
+}
+
