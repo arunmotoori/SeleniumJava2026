a2bb102e7a453c98cd9d23273fe03ecdc3c39eed
Arun Motoori <arun.motoori@gmail.com>
2026-08-03 20:30:58 +0530

    System.getProperty() in java

---
+++ 179. System.getProperty() in Java/DemoD.java
@@ -0,0 +1,20 @@
+package filehandlingpack;
+
+public class DemoD {
+
+	public static void main(String[] args) {
+		
+		String prop1Value = System.getProperty("user.dir");
+		System.out.println(prop1Value);
+		
+		System.out.println(System.getProperty("user.name"));
+		
+		System.out.println(System.getProperty("os.name"));
+		
+		System.out.println(System.getProperty("java.version"));
+
+	}
+
+}
+
+
